1a49e77df945623a106f58517b8a7d96e2eb72d9
Waldener Monteiro <jjrr019@gmail.com>
2020-08-08 20:51:48 -0300

    refactor: utilizando a minha api

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const HTTPClient = axios.create({
-  baseURL: 'https://cors-anywhere.herokuapp.com/http://api.todo.ootz.com.br/',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Cache-Control': 'no-cache',
-    Pragma: 'no-cache',
-    apiKey: 'B08392ECA350755DBE7CC9C7D17BF2F56BCE9EF0AE2CBF84B7C01CC5D4CE180A'
+    Pragma: 'no-cache'
+    // apiKey: 'B08392ECA350755DBE7CC9C7D17BF2F56BCE9EF0AE2CBF84B7C01CC5D4CE180A'
   }
 })
 
