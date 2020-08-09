f4a64b723a14647c42419cf9aa03bdcb17f87e5b
Waldener Monteiro <jjrr019@gmail.com>
2020-08-08 21:38:29 -0300

    refactor(frames): retirando ordenações desnecessárias

--- src/pages/frames/store/mutations.js
+++ src/pages/frames/store/mutations.js
@@ -1,10 +1,3 @@
 export const SET_DATA = (state, payload) => {
-  state.frames = payload.sort(function (a, b) {
-    return a.position < b.position ? -1 : 1
-  })
-  for (const frame of state.frames) {
-    frame.tasks = frame.tasks.sort(function (a, b) {
-      return a.position < b.position ? -1 : 1
-    })
-  }
+  state.frames = payload
 }
