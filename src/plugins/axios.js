9464ea9c800bd0182f18775a132ae113f140b4a1
Waldener Monteiro <jjrr019@gmail.com>
2020-08-09 02:26:18 -0300

    refactor: adicionando endereço de produção da api

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const HTTPClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://personal-board-node-api.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
